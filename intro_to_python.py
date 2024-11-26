cd5a67b6d045eac4834b49f0b35b5ec780d83ae8
pedrocruz <JayJMarquez05@gmail.com>
2024-11-26 11:54:29 -0600

    Forgot to commit eariler but this is the offical first commit (in the course) for python

---
+++ intro_to_python.py
@@ -0,0 +1,37 @@
+# This is a comment. Python will ignore this. Just like JS
+
+"""
+This is how you comment out multiple lines. Triple Double Quotes!
+"""
+
+'''
+That also includes triple single quotes as well.
+These two examples of multi lined comments are referred to as doc strings.
+Basically in a nut shell. This is used for documenting 
+'''
+
+# This is the console.log(`Hello World!`) from JS but now down below is the python variation.
+print('Hello World!')
+
+# In order to get the console to run our file. We call " python3 file_name "
+
+num = 15
+
+# python's variables are dynamic
+
+# movie | If I declare a non variable. Python will mark it as wrong. Why?
+# Because python doesn't have " Undefined " as a data type.
+
+movie = 'Sonic'
+
+# This is how you do casing in python. Unlike JS where it's camelCasing.
+# Here we use snake_casing
+my_number = 10
+
+print(my_number)
+
+my_number = -5
+
+print(my_number)
+
+# We can redeclare/reassign variables over and over again just like in JS
